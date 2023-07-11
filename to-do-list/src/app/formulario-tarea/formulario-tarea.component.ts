0a232ff62fc98ba165f0911c97fdb6d5724ae3da
GitJanPlata <96839905+GitJanPlata@users.noreply.github.com>
2023-07-11 19:41:38 +0200

    comentarios añadidos

--- to-do-list/src/app/formulario-tarea/formulario-tarea.component.ts
+++ to-do-list/src/app/formulario-tarea/formulario-tarea.component.ts
@@ -1,23 +1,28 @@
+// Este es el componente del formulario de la tarea. Aquí es donde se crea una nueva tarea.
+
 import { Component, EventEmitter, Output } from '@angular/core';
 
+// Decorador que especifica los metadatos del componente.
 @Component({
-  selector: 'app-formulario-tarea',
-  templateUrl: './formulario-tarea.component.html',
-  styleUrls: ['./formulario-tarea.component.css']
+  selector: 'app-formulario-tarea', // El nombre del componente.
+  templateUrl: './formulario-tarea.component.html', // La ubicación del archivo de plantilla HTML.
+  styleUrls: ['./formulario-tarea.component.css'] // La ubicación del archivo de estilos CSS.
 })
-export class FormularioTareaComponent {
-  @Output() tareaCreada = new EventEmitter<any>();
-  titulo:string = '';
-  descripcion:string = '';
 
+export class FormularioTareaComponent {
+  @Output() tareaCreada = new EventEmitter<any>(); // Esto emitirá un evento cada vez que se cree una nueva tarea.
+  titulo:string = ''; // Aquí se almacena el título de la tarea.
+  descripcion:string = ''; // Aquí se almacena la descripción de la tarea.
+
+  // Este método crea una nueva tarea y la emite.
   agregarTarea() {
-    const tarea = {
+    const tarea = { // Crea una nueva tarea.
       titulo: this.titulo,
       descripcion: this.descripcion,
       completada: false
     };
-    this.tareaCreada.emit(tarea);
-    this.titulo = '';
-    this.descripcion = '';
+    this.tareaCreada.emit(tarea); // Emite la nueva tarea.
+    this.titulo = ''; // Limpia el campo del título.
+    this.descripcion = ''; // Limpia el campo de la descripción.
   }
 }
--- to-do-list/src/app/lista-tareas/lista-tareas.component.ts
+++ to-do-list/src/app/lista-tareas/lista-tareas.component.ts
@@ -1,5 +1,8 @@
+// Este es el componente de la lista de tareas. Aquí es donde se almacenan, se completan y se eliminan las tareas.
+
 import { Component } from '@angular/core';
 
+// Esto es una interfaz para el tipo de datos Tarea.
 interface Tarea {
   titulo: string;
   descripcion: string;
@@ -7,21 +10,25 @@ interface Tarea {
 }
 
 @Component({
-  selector: 'app-lista-tareas',
-  templateUrl: './lista-tareas.component.html',
-  styleUrls: ['./lista-tareas.component.css']
+  selector: 'app-lista-tareas', // El nombre del componente.
+  templateUrl: './lista-tareas.component.html', // La ubicación del archivo de plantilla HTML.
+  styleUrls: ['./lista-tareas.component.css'] // La ubicación del archivo de estilos CSS.
 })
-export class ListaTareasComponent {
-  tareas: Tarea[] = [];
 
+export class ListaTareasComponent {
+  tareas: Tarea[] = []; // Aquí es donde se almacenan las tareas.
+
+  // Este método agrega una nueva tarea a la lista de tareas.
   agregarTarea(tarea: Tarea) {
     this.tareas.push(tarea);
   }
 
+  // Este método marca una tarea como completada o no completada.
   completarTarea(index: number) {
     this.tareas[index].completada = !this.tareas[index].completada;
   }
 
+  // Este método elimina una tarea de la lista de tareas.
   eliminarTarea(index: number) {
     this.tareas.splice(index, 1);
   }
